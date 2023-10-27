19787281e973b8d9dfd2ea1a5aeb16e6c33f59a9
Bart-Nikita <info@bart-group.com>
2023-10-28 00:52:59 +0800

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { graphql, Link, type HeadFC, type PageProps } from "gatsby"
 import Seo from "../components/seo/Seo"
 import LoadWhenInView from "../components/LoadWhenInView/LoadWhenInView"
-
+const Test = React.lazy(() => import("../components/test/Test"))
 export const Head = ({ data }: PageProps<Queries.IndexPageQuery>) => {
   return (
     <Seo favicon={data.wpMenu?.favicon?.faviconIkonka?.sourceUrl || ''} url={data?.site?.siteMetadata?.url || ''} title={'Брихад-Мриданга | Главная'} description={'Свежие переводы фундаментальных трудов гаудия-вайшнавов на русский язык'}></Seo>)
@@ -11,25 +11,26 @@ export const Head = ({ data }: PageProps<Queries.IndexPageQuery>) => {
 const IndexPage = ({ data }: PageProps<Queries.IndexPageQuery>) => {
   return (
     <section className="container" >
-      <LoadWhenInView>
       <h1 className="text-2xl font-bold mx-0 uppercase w-fit">Труды гаудия-вайшнавов на русском</h1>
-      {data?.wpCategory?.wpChildren?.nodes?.sort((a,b) => a.order.poryadkovyjNomer < b.order.poryadkovyjNomer ? -1 : 1)?.map((author, index) => <div className="mt-5" key={author.id}>
+      {data?.wpCategory?.wpChildren?.nodes?.sort((a, b) => a.order.poryadkovyjNomer < b.order.poryadkovyjNomer ? -1 : 1)?.map((author, index) => <div className="mt-5" key={author.id}>
         <Link className="w-fit link-animation-opacity" to={`/${author.slug}`}>
           <h2 className="text-lg font-bold uppercase  w-fit">{author.name}</h2>
         </Link>
         <ul className="list-none  mt-2">
-          {author?.wpChildren?.nodes.sort((a,b) => a.order.poryadkovyjNomer < b.order.poryadkovyjNomer ? -1 : 1).map((book, index) => <li key={index}>
+          {author?.wpChildren?.nodes.sort((a, b) => a.order.poryadkovyjNomer < b.order.poryadkovyjNomer ? -1 : 1).map((book, index) => <li key={index}>
             <Link className="w-fit link-animation-opacity" to={`/${author.slug}/${book.slug}`}>
               <h3 className="text-md font-medium  w-fit">{book.name}</h3>
             </Link>
             <ul className="list-none pl-3">
-              {book.posts?.nodes.sort((a,b) => a.date < b.date ? -1 : 1)?.map((chapter, index) => <li key={chapter.slug}>
+              {book.posts?.nodes.sort((a, b) => a.date < b.date ? -1 : 1)?.map((chapter, index) => <li key={chapter.slug}>
                 <Link className="text-sm mt-1 link-animation-opacity" to={`/${author.slug}/${book.slug}/${chapter.slug}`}>{chapter.title}</Link>
               </li>)}
             </ul>
           </li>)}
         </ul>
       </div>)}
+      <LoadWhenInView>
+        <Test></Test>
       </LoadWhenInView>
     </section>
   )
